08879ac3bba5fe05f2114d02680423af363f35cc
Mohamed Samir Salem <mohamedkhalilalmasri@gmail.com>
2024-02-10 11:31:19 +0200

    banners repository

--- lib/data/model/banners/banners_model.dart
+++ lib/data/model/banners/banners_model.dart
@@ -2,12 +2,14 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'banners_model.g.dart';
 
+/// Model class representing banners data.
 @JsonSerializable()
 class BannersModel {
   bool? status;
   dynamic message;
-  List<Data> ?  data;
+  List<Data>? data;
 
+  /// Constructor for BannersModel.
   BannersModel({this.status, this.message, this.data});
 
   /// Creates an instance of [BannersModel] from a JSON map.
@@ -18,6 +20,7 @@ class BannersModel {
   Map<String, dynamic> toJson() => _$BannersModelToJson(this);
 }
 
+/// Model class representing individual banner data.
 @JsonSerializable()
 class Data {
   int? id;
@@ -25,9 +28,10 @@ class Data {
   dynamic category;
   dynamic product;
 
+  /// Constructor for Data.
   Data({this.category, this.id, this.image, this.product});
 
-  /// Creates an instance of [BannersModel] from a JSON map.
+  /// Creates an instance of [Data] from a JSON map.
   factory Data.fromJson(Map<String, dynamic> json) => _$DataFromJson(json);
 
   /// Converts this object into a JSON-compatible map.
---
+++ lib/data/repository/banner_repository.dart
@@ -0,0 +1,39 @@
+import 'package:dio/dio.dart';
+import 'package:stylish_ecommerce/data/model/api_result/api_result.dart';
+import 'package:stylish_ecommerce/data/model/banners/banners_model.dart';
+import 'package:stylish_ecommerce/data/model/network_exception/network_exception.dart';
+import 'package:stylish_ecommerce/data/web_services/get.dart';
+
+/// Repository class responsible for fetching banners data.
+class BannerRepository {
+  final Dio dio; // Dio instance for making HTTP requests.
+  final GetMethod getMethod; // Method for making GET requests.
+
+  /// Constructor for BannerRepository.
+  /// [dio]: Dio instance for making HTTP requests.
+  /// [getMethod]: Method for making GET requests.
+  const BannerRepository({required this.dio, required this.getMethod});
+
+  /// Fetches banner data from the server.
+  ///
+  /// Returns an [ApiResult] containing [BannersModel] if successful,
+  /// or a failure result containing a [NetworkExceptions] if an error occurs.
+  Future<ApiResult<BannersModel>> getBanner() async {
+    try {
+      // Make a GET request to fetch banner data.
+      var response = await getMethod.doHttpMethod(
+        dio: dio,
+        uri: '/banners',
+      );
+
+      // Parse the response data into BannersModel.
+      BannersModel data = BannersModel.fromJson(response.data);
+
+      // Return success result with the fetched data.
+      return ApiResult.success(data);
+    } catch (error) {
+      // If an error occurs during the request, return a failure result with the appropriate exception.
+      return ApiResult.failure(NetworkExceptions.getDioException(error));
+    }
+  }
+}
